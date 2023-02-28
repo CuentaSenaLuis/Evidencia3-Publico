47c7b6ec029d1484085a725b50320b86b0051d27
CuentaSenaLuis <125584846+CuentaSenaLuis@users.noreply.github.com>
2023-02-27 23:55:13 -0500

    Ev1

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')#Es carpeta raiz
+
+def index():
+    titulos = ['Home', 'Registro', 'Inicio Sesion']
+    data = {
+        'Titulos': titulos
+    }
+    return render_template('index.html', data=data)
+
+@app.route('/Cuenta/Login')
+def Login():
+    titulos = ['Home', 'Registro', 'Inicio Sesion']
+    data={
+        'Titulos': titulos
+    }
+    return render_template('Login.html', data=data)
+
+@app.route('/Cuenta/Registro')
+def Register():
+    titulos = ['Home', 'Registro', 'Inicio Sesion']
+    data={
+        'Titulos': titulos
+    }
+    return render_template('Registro.html', data=data)
+
+if __name__ == '__main__':
+    app.run()
+    
