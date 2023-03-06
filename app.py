a7b9c8c67d10b4dbb70e7ca879481d1bd8e6868a
CuentaSenaLuis <125584846+CuentaSenaLuis@users.noreply.github.com>
2023-03-06 11:26:43 -0500

    Ev 2.1

--- app.py
+++ app.py
@@ -2,31 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/Cuenta/Home')#Es carpeta raiz
+@app.route('/')#Es carpeta raiz
 
 def index():
-    titulos = ['Home', 'Registro', 'Inicio Sesion']
     data = {
-        'Titulos': titulos
+        'Titulos': 'Home'
     }
     return render_template('index.html', data=data)
 
 @app.route('/Cuenta/Login')
 def Login():
-    titulos = ['Home', 'Registro', 'Inicio Sesion']
     data={
-        'Titulos': titulos
+        'Titulos': 'Inicio Sesion'
     }
     return render_template('Login.html', data=data)
 
 @app.route('/Cuenta/Registro')
 def Register():
-    titulos = ['Home', 'Registro', 'Inicio Sesion']
     data={
-        'Titulos': titulos
+        'Titulos': 'Registro' 
     }
     return render_template('Registro.html', data=data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
     
