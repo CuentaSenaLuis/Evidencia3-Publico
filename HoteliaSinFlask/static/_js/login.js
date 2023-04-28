e08ee7d588a2198cb157e3ced65345130c84d2ef
CuentaSenaLuis <125584846+CuentaSenaLuis@users.noreply.github.com>
2023-04-27 19:12:31 -0500

    Login Changes

--- HoteliaSinFlask/static/_js/login.js
+++ HoteliaSinFlask/static/_js/login.js
@@ -7,12 +7,34 @@ const validarForm = (e) => {
     switch (e.target.name){
         case "correo":
             if(validator.test(e.target.value)){
-                document.getElementById('prueba').classList.add('icon-correct');
+                document.getElementById('txtcorreo').classList.remove('txt_field');
+                document.getElementById('lblcorreo').classList.add('txt_field-up');
+                document.getElementById('txtcorreo').classList.add('txt_field-correct');
             }else if(e.target.value == ""){
+                document.getElementById('txtcorreo').classList.remove('txt_field-correct');
                 document.getElementById('lblcorreo').classList.remove('txt_field-up');
+                document.getElementById('txtcorreo').classList.add('txt_field');
             } 
             else{
+                document.getElementById('txtcorreo').classList.remove('txt_field-correct');
                 document.getElementById('lblcorreo').classList.add('txt_field-up');
+                document.getElementById('txtcorreo').classList.add('txt_field');
+            }
+        break;
+        case "password":
+            if(e.target.value != ""){
+                document.getElementById('txtpassword').classList.remove('txt_field');
+                document.getElementById('lblpassword').classList.add('txt_field-up');
+                document.getElementById('txtpassword').classList.add('txt_field-correct');
+            }else if(e.target.value == ""){
+                document.getElementById('txtpassword').classList.remove('txt_field-correct');
+                document.getElementById('lblpassword').classList.remove('txt_field-up');
+                document.getElementById('txtpassword').classList.add('txt_field');
+            } 
+            else{
+                document.getElementById('txtpassword').classList.remove('txt_field-correct');
+                document.getElementById('lblpassword').classList.add('txt_field-up');
+                document.getElementById('txtpassword').classList.add('txt_field');
             }
         break;
     };
